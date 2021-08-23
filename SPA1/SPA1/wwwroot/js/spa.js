f659e62dafdb1b66e40e5cc7a85f6a68671f01ea
LAPTOP-NG4TP1P4\mattm <mattmaton@hotmail.co.uk>
2021-08-23 14:50:02 +0100

    Inital commit

---
+++ SPA1/SPA1/wwwroot/js/spa.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('#indexpanel').click(function () {
+        $('#page-content-wrapper').load('trail1.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#experience').click(function () {
+        $('#page-content-wrapper').load('experience.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#freelance').click(function () {
+        $('#page-content-wrapper').load('freelance.html');
+    })
+});
