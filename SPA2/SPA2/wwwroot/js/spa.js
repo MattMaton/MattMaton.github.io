3e0b228eaffc864362791fafe7afed2d9f77fd3d
LAPTOP-NG4TP1P4\mattm <mattmaton@hotmail.co.uk>
2021-08-29 20:33:56 +0100

    Update to file layout

---
+++ SPA2/SPA2/wwwroot/js/spa.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#indexpanel').click(function () {
+        $('#page-content-wrapper').load('trail1.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#experience').click(function () {
+        $('#page-content-wrapper').load('experience.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#freelance').click(function () {
+        $('#page-content-wrapper').load('freelance.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#prevprojects').click(function () {
+        $('#page-content-wrapper').load('prev-projects.html');
+    })
+});
